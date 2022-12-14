4d7459338d13456afe969402796e5cdd9b831a36
thomas <tom.angotti11@gmail.com>
2022-12-13 17:39:08 -0700

    commit tree aa6e84d7297c32408b4543801d17fd1ab9f28849

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('#main').remove();
+
+const newHeader = document.createElement('h1');
+document.body.append(newHeader);
+newHeader.id = 'victory';
+newHeader.innerText = "Thomas Angotti is the champion"
